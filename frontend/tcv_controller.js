d0ad36a170b8d140a4eb30d3b66a296e14f6c108
herrkami <aebsor@gmail.com>
2024-01-29 12:54:47 +0100

    Add config variables and improve message handlers

--- frontend/tcv_controller.js
+++ frontend/tcv_controller.js
@@ -1,6 +1,7 @@
 
 // import { WebSocket } from 'ws';
 // WebSocket = require('ws');
+import { Viewer, Timer } from "./three-cad-viewer.esm.js";
 
 const MessageType = {
   "DATA": "D",
@@ -12,6 +13,8 @@ const MessageType = {
 export class TCVController {
 
   constructor(b_three_d_host, b_three_d_port) {
+
+    // WebSocket configuration 
     if (!b_three_d_host) {
       b_three_d_host = 'ws://localhost';
     }
@@ -24,82 +27,32 @@ export class TCVController {
     const url = this.b_three_d_host + ':' + this.b_three_d_port + '/';
     this.socket = new WebSocket(url);
 
-    this.socket.addEventListener('open', function (event) {
+    // On open
+    this.socket.addEventListener('open', (event) => {
       console.debug('TCVController connected.');
     });
-
-    this.socket.addEventListener('message', function (event) {
+    
+    // On message
+    this.socket.addEventListener('message', (event) => {
       const raw_data = event.data;
-      const msg_type = raw_data.substring(0, 1);
-      var msg_data = raw_data.toString().substring(2);
-      // console.debug(`Msg data:\n[${msg_data}]`);
-      // console.debug(`Msg type:\n[${msg_type}]`);
-      if (msg_type === MessageType.CONFIG) {
-        var config = JSON.parse(msg_data);
-        console.log('Received config:', config);
-        // if (data === "status") {
-        //   socket.send(this.viewer_message);
-        // } else if (data === "config") {
-        //   socket.send(JSON.stringify(this.config()));
-        // }
+      var msg = {
+        'type':raw_data.substring(0, 1), 
+        'data': raw_data.toString().substring(2),
+      };
+      switch (msg.type) {
+        case MessageType.CONFIG:
+          this.#handleConfigReceived(msg.data);
+          break;
+        case MessageType.DATA:
+          this.handleDataReceived(msg.data);
+          break;
       }
-
-      // } else if (messageType === "D") {
-      //     output.debug("Received a new model");
-      //     this.view?.postMessage(data);
-      //     output.debug("Posted model to view");
-      //     if (this.splash) { this.splash = false }
-
-      // } else if (messageType === "S") {
-      //     output.debug("Received a config");
-      //     this.view?.postMessage(data);
-      //     output.debug("Posted config to view");
-
-      // } else if (messageType === "L") {
-      //     this.pythonListener = socket;
-      //     output.debug("Listener registered");
-      // }
-      // else if (messageType === "B") {
-      //     this.pythonListener?.send(data);
-      //     output.debug("Model data sent to the backend");
-      // }
-      // else if (messageType === "R") {
-      //     this.view?.postMessage(data);
-      //     output.debug("Backend response received.");
-      // }
-      // } catch (error) {
-      //   console.error(`Server error: ${msg}`);
-      // } 
-
-      // // const f = document.getElementById("chatbox").contentDocument;
-      // // let text = "";
-      // const msg = JSON.parse(event.data);
-      // // const time = new Date(msg.date);
-      // // const timeStr = time.toLocaleTimeString();
-
-      // // Message structure
-      // // msg.type
-      // // msg.action
-      // // msg.data
-      // console.log(`Raw message:\n${msg}`);
-      // console.log(`Received message of type ${msg.type} with data\n${msg.data}`);
-      // switch (msg.type) {
-      //   case MessageType.DATA:
-      //     const data = msg.data;
-      //     handleDataReceived(data);
-      //     break;
-      //   case MessageType.CONFIG:
-      //     const config = msg.data;
-      //     handleConfigUpdate(config);
-      //     break;
-      //   case MessageType.BACKEND_RESPONSE:
-      //     const response = msg.data;
-      //     handleBackendResponse(response);
-      //     break;
-      // }
     });
+
+    // Viewer
   }
 
+  // WebSocket
   isConnectected() {
     if (this.socket.readyState == this.socket.OPEN) {
       return true;
@@ -110,37 +63,75 @@ export class TCVController {
     this.socket.send(msg);
   }
 
+  // Controller
+  #handleConfigReceived(data) {
+    console.debug("#handleConfigReceived called.");
+    var config = JSON.parse(data);
+    console.debug("Received config:", config);
+    // Structure
+    // component.option.value
+    switch (config[0]) {
+      case 'viewer':
+        console.debug("Configuring viewer:", config['viewer']);
+
+        // axes
+        // axes0
+        // grid
+        // ortho
+        // transparent
+        // black_edges
+        // collapse
+        
+        // clip_intersection
+        // clip_plane_helpers
+        // clip_normal
+        // clip_slider
+        // control
+        // ticks
+        
+        // position
+        // quaternion
+        
+        // zoom
+        
+        // pan_speed
+        // rotate_speed
+        // zoom_speed
+        
+        // timeit
+        // zoom0
+        // tools
+        break;
+      case 'display':
+        console.debug("Configuring display:", config['display']);
+        // theme
+        // cad_width
+        // tree_width
+        // height
+        // pinning
+        // glass
+        // tools
+        // keymap
+        break;
+        case 'render':
+        console.debug("Configuring renderer:", config['render']);
+        // ambient_intensity
+        // direct_intensity
+        // metalness
+        // roughness
+        // default_opacity
+        // edge_color
+        // normal_len
+        // measureTools
+
+        break;
+    }
+    // console.log('Received config:', config);
+  }
+
+
   handleDataReceived(data) {
-
-    console.debug("handleDataReceived called.")
+    console.debug("handleDataReceived called.");
     return;
   }
-
-  handleConfigUpdate(config) {
-    console.debug("handleConfigUpdate called.")
-    return;
-  }
-
-  handleBackendResponse(response) {
-    console.debug("handleBackendResponse called.")
-    return;
-  }
-
-  // exampleSocket.onmessage = (event) => {
-
 }
-
-// const socket = new WebSocket('ws://localhost:9876');
-
-// socket.addEventListener('open', function (event) {
-//   socket.send('Connection Established');
-// });
-
-// socket.addEventListener('message', function (event) {
-//   console.debug(event.data);
-// });
-
-// const pingTheServer = () => {
-//   // console.log("Here we are.")
-//   socket.send("Ping?");
-// }
--- tcv/_generate_options.py
+++ tcv/_generate_options.py
@@ -3,11 +3,6 @@ import re
 
 options = {
     "viewer": {
-        "control": {
-            "type": 'str',
-            "default": "orbit",
-            "description": 'Viewpoint control ["orbit", "trackball"] (use "orbit" for mouse)',
-        },
         "axes": {
             "type": 'bool',
             "default": False,
@@ -38,6 +33,12 @@ options = {
             "default": False,
             "description": 'Show edges in black and instead of edgeColor',
         },
+        "collapse": {
+            "type": 'int',
+            "default": 0,
+            "description": 'Tree collapse level (0 .. 3)',
+        },
+
         "clipIntersection": {
             "type": 'bool',
             "default": False,
@@ -48,15 +49,50 @@ options = {
             "default": False,
             "description": 'Show clipping planes',
         },
-        "clipNormal": {
+        "clipNormal": {  # clipNormal<0,1,2>
             "type": '[[int, int, int], [int, int, int], [int, int, int]]',
             "default": [[-1, 0, 0], [0, -1, 0], [0, 0, -1]],
             "description": 'Normal directions for clipping',
         },
+        "clipSlider": {  # clipSlider<0,1,2>
+            "type": '[int, int, int]',
+            "default": [-1, -1, -1],
+            "description": "Clipping slider value (will be trimmed to slider's min/max limits)",
+        },
+        "control": {
+            "type": 'str',
+            "default": "orbit",
+            "description": 'Viewpoint control ["orbit", "trackball"] (use "orbit" for mouse)',
+        },
+        # up
         "ticks": {
             "type": 'int',
             "default": 10,
             "description": 'Number of grid ticks',
+        },
+
+        "position": {
+            "type": '[float, float, float]',
+            "default": None,
+            "description": 'Camera position [x, y, z]',
+        },
+        "quaternion": {
+            "type": '[float, float, float, float]',
+            "default": None,
+            "description": 'Camera rotation as 4-dim quaternion array [x, y, z, w]',
+        },
+        # target
+
+        "zoom": {
+            "type": 'float',
+            "default": 1.0,
+            "description": 'Camera zoom factor',
+        },
+
+        "panSpeed": {
+            "type": 'float',
+            "default": 0.5,
+            "description": 'Pan speed',
         },
         "rotateSpeed": {
             "type": 'float',
@@ -68,26 +104,12 @@ options = {
             "default": 0.5,
             "description": 'Zoom speed',
         },
-        "panSpeed": {
-            "type": 'float',
-            "default": 0.5,
-            "description": 'Pan speed',
+        "timeit": {
+            "type": 'bool',
+            "default": False,
+            "description": 'Show timings in browser console',
         },
-        "position": {
-            "type": '[float, float, float]',
-            "default": None,
-            "description": 'Camera position [x, y, z]',
-        },
-        "quaternion": {
-            "type": '[float, float, float, float]',
-            "default": None,
-            "description": 'Camera rotation as 4-dim quaternion array [x, y, z, w]',
-        },
-        "zoom": {
-            "type": 'float',
-            "default": 1.0,
-            "description": 'Camera zoom factor',
-        },
+
         "zoom0": {
             "type": 'float',
             "default": 1.0,
@@ -98,42 +120,52 @@ options = {
             "default": True,
             "description": 'Show/hide all tools',
         },
-        "timeit": {
-            "type": 'bool',
-            "default": False,
-            "description": 'Show timings in browser console',
-        },
     },
     
     "display": {
+        "theme": {
+            "type": 'str',
+            "default": "light",
+            "description": 'Theme ["light", "dark"]',
+        },
         "cadWidth": {
             "type": 'int',
             "default": 800,
             "description": 'Width of the CAD canvas',
-        },
-        "height": {
-            "type": 'int',
-            "default": 600,
-            "description": 'Height of the CAD canvas',
         },
         "treeWidth": {
             "type": 'int',
             "default": 250,
             "description": 'Width of the tree section',
         },
-        "theme": {
-            "type": 'str',
-            "default": "light",
-            "description": 'Theme ["light", "dark"]',
+        "height": {
+            "type": 'int',
+            "default": 600,
+            "description": 'Height of the CAD canvas',
+        },
+        "pinning": {
+            "type": 'bool',
+            "default": False,
+            "description": 'Show/hide the pinning button',
+        },
+        "glass": {
+            "type": 'bool',
+            "default": False,
+            "description": 'Enable/disable glass mode (i.e. transparent tree)',
+        },
+        "tools": {
+            "type": 'bool',
+            "default": True,
+            "description": 'Show/hide CAD tools',
+        },
+        "keymap": {
+            "type": 'dict',
+            "default": { "shift": "shiftKey", "ctrl": "ctrlKey", "meta": "metaKey" },
+            "description": 'Keymap configuration for the modifier keys',
         },
     },
     
     "render": {
-        "edgeColor": {
-            "type": 'int',
-            "default": 0x707070,
-            "description": 'Default edge color in hex format',
-        },
         "ambientIntensity": {
             "type": 'float',
             "default": 0.5,
@@ -144,15 +176,35 @@ options = {
             "default": 0.3,
             "description": 'Direct light intensity',
         },
+        "metalness": {
+            "type": 'float',
+            "default": 0.7,
+            "description": 'Metalness of the CAD object',
+        },
+        "roughness": {
+            "type": 'float',
+            "default": 0.7,
+            "description": 'Roughness of the CAD object',
+        },
         "defaultOpacity": {
             "type": 'float',
             "default": 0.4,
             "description": 'Default opacity level in transparency mode',
         },
+        "edgeColor": {
+            "type": 'int',
+            "default": 0x707070,
+            "description": 'Default edge color in hex format (might be displayed in decimal here: hex(7368816) = 0x707070)',
+        },
         "normalLen": {
             "type": 'float',
             "default": 0,
             "description": 'Show triangle normals when normalLen > 0',
+        },
+        "measureTools": {
+            "type": 'bool',
+            "default": False,
+            "description": 'Show/hide measurement tools',
         },
     }
 }
@@ -171,18 +223,29 @@ def snake_to_camel(name):
     return camel
 
 if __name__ == '__main__':
-    for group in options.keys():
-        for opt in options[group].keys():
-            options[group][opt]['snake_cased'] = camel_to_snake(opt)
+    for component in options.keys():
+        for opt in options[component].keys():
+            options[component][opt]['snake_cased'] = camel_to_snake(opt)
             try:
-                camel = snake_to_camel(options[group][opt]['snake_cased'])
+                camel = snake_to_camel(options[component][opt]['snake_cased'])
                 assert(opt == camel)
-                options[group][opt]['camel_cased'] = camel
+                options[component][opt]['camel_cased'] = camel
             except:
                 e = 'Option key {} changes when camel cased: {}'.format(opt, camel)
                 raise AssertionError(e)
     
     with open('options.json', 'w') as f:
         json.dump(options, f, indent=2)
+
+    with open('options.md', 'w') as f:
+        content = '# Available options\n\n'
+        for component in options.keys():
+            content += f'## `{component}`\n\n'
+            for opt in options[component].keys():
+                content += f'- `{options[component][opt]["snake_cased"]}`: {options[component][opt]["type"]} (default: {options[component][opt]["default"]})<br>'
+                content += f'  {options[component][opt]["description"]}\n'
+            content += '\n'
+                
+        f.write(content)
     
 
